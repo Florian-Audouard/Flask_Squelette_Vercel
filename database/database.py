ebfafcc688a8f1c2282b6576591a42a151284676
Audouard Florian <nairolf.audouard@gmail.com>
2024-10-16 22:52:43 +0200

    clean querry

--- database/database.py
+++ database/database.py
@@ -7,7 +7,7 @@ import os
 import urllib.parse
 import psycopg2
 from dotenv import dotenv_values
-
+from time import sleep
 os.chdir(os.path.dirname(__file__))
 
 default_look_filename = ".env"
@@ -34,31 +34,39 @@ options = urllib.parse.quote_plus("--search_path=modern,public")
 CONN_PARAMS = f"postgresql://{config['USER']}:{config['PASSWORD']}@{config['HOST']}:{config['PORT']}/{config['DATABASE']}?options={options}"  # pylint: disable=line-too-long
 
 
-def reset_table():  # pylint: disable=missing-function-docstring
-    with psycopg2.connect(CONN_PARAMS) as conn:  # pylint: disable=not-context-manager
-        with conn.cursor() as cur:
-            with open(FILENAME_DB_SHEMA, "r", encoding="utf-8") as file:
-                cur.execute(file.read())
+def clean_querry(func):
+    def wrapper_func(*args, **kwargs):
+        res = ""
+        postgres = psycopg2.connect(CONN_PARAMS)
+        with postgres as conn:  # pylint: disable=not-context-manager
+            with conn.cursor() as cur:
+                res = func(cur,*args, **kwargs)
+        postgres.close()
+        return res
+    return wrapper_func
 
 
-def get_data():  # pylint: disable=missing-function-docstring
-    with psycopg2.connect(CONN_PARAMS) as conn:  # pylint: disable=not-context-manager
-        with conn.cursor() as cur:
-            cur.execute("select text from data;")
-            return cur.fetchall()
+@clean_querry
+def reset_table(cur):  # pylint: disable=missing-function-docstring
+    with open(FILENAME_DB_SHEMA, "r", encoding="utf-8") as file:
+        cur.execute(file.read())
 
+@clean_querry
+def get_data(cur):  # pylint: disable=missing-function-docstring
+    cur.execute("select text from data;")
+    return cur.fetchall()
 
-def init_data():
-    with psycopg2.connect(CONN_PARAMS) as conn:  # pylint: disable=not-context-manager
-        with conn.cursor() as cur:
-            cur.execute(
-                "INSERT INTO data (text) VALUES (%(msg)s);",
-                {
-                    "msg": "Message recu depuis la base de données",
-                },
-            )
+@clean_querry
+def init_data(cur):
+    cur.execute(
+        "INSERT INTO data (text) VALUES (%(msg)s);",
+        {
+            "msg": "Message recu depuis la base de données",
+        },
+    )
 
 
 if __name__ == "__main__":
     reset_table()
     init_data()
+    print(get_data())
